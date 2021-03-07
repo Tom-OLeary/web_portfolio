4dddb989ee22759b93ff384ab551eedaee65b639
Tom O'Leary <Tom@Tom-Mac.local>
2021-03-06 21:55:35 -0500

    slight cleanup

--- blog_fe/src/components/BlogItems.js
+++ blog_fe/src/components/BlogItems.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../css/BlogItems.css';
 import '../css/Header.css';
 import $ from "jquery";
--- blog_fe/src/components/Trivia.js
+++ blog_fe/src/components/Trivia.js
@@ -39,11 +39,8 @@ function Trivia(props) {
     return (
         <div className="trivia-left">
         <div className="modal-container" id="score-modal">
-
             <div className="modal-content-container">
-
                 <h1>Congratulations, Quiz Completed.</h1>
-
                 <div className="grade-details">
                     <p>Attempts : 10</p>
                     <p>Wrong Answers : <span id="wrong-answers"></span></p>
@@ -51,71 +48,49 @@ function Trivia(props) {
                     <p>Grade : <span id="grade-percentage"></span>%</p>
                     <p ><span id="remarks"></span></p>
                 </div>
-
                 <div className="modal-button-container">
                     <button onClick={closeScoreModal}>Continue</button>
                 </div>
-
             </div>
         </div>
-
         <div className="game-quiz-container">
-
             <div className="game-details-container">
                 <h1>Score : <span id="player-score"></span> / 10</h1>
                 <button onClick={() => NextQuestion(0, questions)} id="play-button">Play Trivia</button>
                 <h1> Question : <span id="question-number"></span> / 10</h1>
             </div>
-
             <div className="game-question-container">
                 <h1 id="display-question"></h1>
             </div>
-
             <div className="game-options-container">
-
                 <div className="modal-container" id="option-modal">
-
                     <div className="modal-content-container">
                         <h1>Please Pick An Option</h1>
-
                         <div className="modal-button-container">
                             <button onClick={closeOptionModal}>Continue</button>
                         </div>
-
                     </div>
-
                 </div>
-
                 <span>
                     <input type="radio" id="option-one" name="option" className="radio" value="1" />
                     <label htmlFor="option-one" className="option" id="option-one-label"></label>
                 </span>
-
-
                 <span>
                     <input type="radio" id="option-two" name="option" className="radio" value="2" />
                     <label htmlFor="option-two" className="option" id="option-two-label"></label>
                 </span>
-
-
                 <span>
                     <input type="radio" id="option-three" name="option" className="radio" value="3" />
                     <label htmlFor="option-three" className="option" id="option-three-label"></label>
                 </span>
-
-
                 <span>
                     <input type="radio" id="option-four" name="option" className="radio" value="4" />
                     <label htmlFor="option-four" className="option" id="option-four-label"></label>
                 </span>
-
-
             </div>
-
             <div className="next-button-container">
                 <button onClick={handleNextQuestion}>Submit</button>
             </div>
-
         </div>
         </div>
     )
