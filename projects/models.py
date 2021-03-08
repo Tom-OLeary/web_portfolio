4ecbe8f5f76e1dc9ff6ff2bfa86cd5ad0ebc7a82
Tom O'Leary <Tom@Tom-Mac.local>
2021-03-07 19:06:07 -0500

    fix tech length

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=200)
     image = models.FilePathField(path="/img")
     condition = models.CharField(max_length=100)
     github = models.URLField(max_length=200, default='')
