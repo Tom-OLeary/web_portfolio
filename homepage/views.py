9dc2d3f8cab340b24d9e79312570e60d2cd11746
Tom O'Leary <Tom@Tom-Mac.local>
2021-03-10 10:19:15 -0500

    minor linting

--- homepage/views.py
+++ homepage/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_index(request):
-    # return render(request, "index.html", {})
     return render(request, "homepage.html", {})
